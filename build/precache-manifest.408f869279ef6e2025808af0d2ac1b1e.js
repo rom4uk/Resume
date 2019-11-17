cf7feac017267795860ff363f161b139a6e74561
ZsuzsaMano <51864343+ZsuzsaMano@users.noreply.github.com>
2019-11-17 18:54:40 +0100

    First react commit

---
+++ build/precache-manifest.408f869279ef6e2025808af0d2ac1b1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d614cdf566ff5e3ae25a1ef9047c95a",
+    "url": "/Resume/index.html"
+  },
+  {
+    "revision": "0cde19503e8c6e1d99c8",
+    "url": "/Resume/static/css/main.072b38e5.chunk.css"
+  },
+  {
+    "revision": "447f2957b65c10491fba",
+    "url": "/Resume/static/js/2.b1bfcbc7.chunk.js"
+  },
+  {
+    "revision": "0cde19503e8c6e1d99c8",
+    "url": "/Resume/static/js/main.5d14eacc.chunk.js"
+  },
+  {
+    "revision": "1485bfbefb266f8f8a18",
+    "url": "/Resume/static/js/runtime-main.d2e35b11.js"
+  }
+]);
